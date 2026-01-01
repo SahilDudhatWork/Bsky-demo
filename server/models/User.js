438d1d33a2c590295a3ca5a16517ea9e9b9f8d11
sahil dudhat <sahildudhat03@gmail.com>
2026-01-01 16:06:38 +0530

    update code

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,17 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, index: true },
     passwordHash: { type: String, required: true },
     bskyHandle: { type: String },
-    bskyAppPasswordEnc: { type: String }
+    bskyAppPasswordEnc: { type: String },
+    // OAuth fields
+    bskyAccessTokenEnc: { type: String },
+    bskyRefreshTokenEnc: { type: String },
+    bskyTokenType: { type: String },
+    bskyExpiresAt: { type: Date },
+    bskyDid: { type: String },
+    bskyAuthServer: { type: String },
+    bskyDpopKeyEnc: { type: String },
+    bskyNonceEnc: { type: String },
+    oauthTempState: { type: Object }
   },
   { timestamps: true }
 )
